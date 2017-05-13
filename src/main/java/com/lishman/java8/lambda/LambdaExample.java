560b1d32f5b124e5d01d146413634a40ed36cb54
marklishman <mark.lishman@gmail.com>
2017-05-13 07:58:29 +0100

    Functional interfaces

--- src/main/java/com/lishman/java8/lambda/LambdaExample.java
+++ src/main/java/com/lishman/java8/lambda/LambdaExample.java
@@ -2,15 +2,19 @@ package com.lishman.java8.lambda;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
 import java.util.function.IntBinaryOperator;
+import java.util.function.IntFunction;
 import java.util.function.IntToLongFunction;
 
 public class LambdaExample {
     
     public static void main(String[] args) {
 //        new LambdaExample().lambda();
-        new LambdaExample().functionalInterfaces();
+//        new LambdaExample().functionalInterfaces();
+        new LambdaExample().iterables();
 //        new LambdaExample().methodReference();
     }
     
@@ -70,7 +74,7 @@ public class LambdaExample {
         Function<Integer, Boolean> even = arg -> arg % 2 == 0;
         System.out.println("Function: " + even.apply(6));
 
-        Function<String, String> reverse = arg -> StringUtils.reverse(arg);
+        Function<String, String> reverse = StringUtils::reverse;
         Function<String, String> halve = arg -> arg.substring(0, arg.length() / 2);
 
         System.out.println("Reverse then halve: " + reverse.andThen(halve).apply("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
@@ -79,20 +83,20 @@ public class LambdaExample {
         System.out.println("Halve then halve again: " + halve.compose(halve).apply ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
 
 
-
-
+        IntFunction<String> intFunction = arg -> "The number is " + arg;
+        System.out.println("IntFunction: " + intFunction.apply(12321));
 
 
         IntToLongFunction square = num -> num * num;
         System.out.println("IntToLongFunction: " + square.applyAsLong(12));
 
-
-        
     }
 
+    private void iterables() {
 
+        Arrays.asList(1,2,3,4,5).forEach(number -> System.out.println(number));
 
-
+    }
 
 
     private void methodReference() {
