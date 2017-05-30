01e73461ae76eab1722bc506cff27f6b46a6ac33
marklishman <mark.lishman@gmail.com>
2017-05-30 20:28:40 +0100

    Stream team data

---
+++ src/main/java/com/lishman/java8/stream/StreamExample.java
@@ -0,0 +1,43 @@
+package com.lishman.java8.stream;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ *  Stream
+ *
+ *      IntStream
+ *      LongStream
+ *      DoubleStream
+ *
+ */
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+
+        teams().stream().forEach(System.out::println);
+
+    }
+
+    private static List<Team> teams() {
+
+        String fileName = "data/world-cup-groups-2014.csv";
+
+        try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
+
+            return stream
+                    .filter(row -> ! row.startsWith("Group"))
+                    .map(row -> Team.builder().row(row).build())
+                    .collect(Collectors.toList());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/lishman/java8/stream/Team.java
@@ -0,0 +1,37 @@
+package com.lishman.java8.stream;
+
+import lombok.Builder;
+import lombok.ToString;
+
+@ToString
+final class Team {
+
+    final public String name;
+    final public String group;
+    final public int position;
+    final public int played;
+    final public int won;
+    final public int drawn;
+    final public int lost;
+    final public int goalsFor;
+    final public int goalsAgainst;
+    final public int goalDifference;
+    final public int points;
+
+    @Builder
+    Team(final String row) {
+        String[] col = row.split(",");
+        group = col[0];
+        position = Integer.parseInt(col[1]);
+        name = col[2];
+        played = Integer.parseInt(col[3]);
+        won = Integer.parseInt(col[4]);
+        drawn = Integer.parseInt(col[4]);
+        lost = Integer.parseInt(col[5]);
+        goalsFor = Integer.parseInt(col[6]);
+        goalsAgainst = Integer.parseInt(col[7]);
+        goalDifference = Integer.parseInt(col[8]);
+        points = Integer.parseInt(col[9]);
+    }
+
+}
