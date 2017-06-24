0c8a400814baea69f8864dbd42e122b6e096ad7b
marklishman <mark.lishman@gmail.com>
2017-06-24 15:37:28 +0100

    Static and default methods

--- src/main/java/io/lishman/functional/functionalinterface/StaticAndDefaultMethods.java
+++ src/main/java/io/lishman/functional/functionalinterface/StaticAndDefaultMethods.java
@@ -5,7 +5,7 @@ import lombok.Builder;
 import java.util.Arrays;
 import java.util.function.Predicate;
 
-public class CustomFunctionalInterface {
+public class StaticAndDefaultMethods {
 
     @FunctionalInterface
     private interface PersonFunction {
