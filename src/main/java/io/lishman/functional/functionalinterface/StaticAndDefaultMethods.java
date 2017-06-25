722e873668fe84e8b3d59a0b38958cdcd6a54d9f
marklishman <mark.lishman@gmail.com>
2017-06-25 10:20:16 +0100

    Static and default methods

--- src/main/java/io/lishman/functional/functionalinterface/StaticAndDefaultMethods.java
+++ src/main/java/io/lishman/functional/functionalinterface/StaticAndDefaultMethods.java
@@ -3,17 +3,23 @@ package io.lishman.functional.functionalinterface;
 import lombok.Builder;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 public class StaticAndDefaultMethods {
 
     @FunctionalInterface
-    private interface PersonFunction {
+    private interface PersonChecker {
 
         Boolean check(Predicate<Person> predicate);
 
         default Boolean isAdult() {
             return check(person -> person.age > 18);
+        }
+
+        default Boolean startsWith(String prefix) {
+            return check(person -> person.name.startsWith(prefix));
         }
     }
 
@@ -22,12 +28,18 @@ public class StaticAndDefaultMethods {
 
         T apply(Person[] people);
 
+        static PeopleFunction<List<Person>> adults() {
+            return people -> Arrays.stream(people)
+                    .filter(Person::isAdult)
+                    .collect(Collectors.toList());
+        }
+
         static PeopleFunction<Integer> count() {
             return people -> people.length;
         }
 
         static PeopleFunction<Double> average() {
-            return people -> Arrays.stream(getPeopleArray())
+            return people -> Arrays.stream(people)
                     .mapToDouble(p -> p.age)
                     .average()
                     .getAsDouble();
@@ -35,7 +47,7 @@ public class StaticAndDefaultMethods {
     }
 
     @Builder
-    static class Person implements PersonFunction {
+    static class Person implements PersonChecker {
         final String name;
         final int age;
 
@@ -45,20 +57,32 @@ public class StaticAndDefaultMethods {
         }
     }
 
-    public static void main(String[] args) {
-        System.out.println("Number of people: " + PeopleFunction.count().apply(getPeopleArray()));
-        System.out.println("Average Age: " + PeopleFunction.average().apply(getPeopleArray()));
-
-        Arrays.stream(getPeopleArray()).forEach(
-                (p) -> System.out.println(p.name + ": " + p.isAdult())
-        );
-    }
-
     private static Person[] getPeopleArray() {
         return new Person[] {
             new Person("Rita", 19),
             new Person("Sue", 21),
             new Person("Bob", 32)
         };
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("\nAdult beginning with S");
+        Arrays.stream(getPeopleArray())
+                .filter(PersonChecker::isAdult)
+                .filter(person -> person.startsWith("S"))
+                .map(p -> p.name)
+                .forEach(System.out::println);
+
+        System.out.println("\nIs Bob?");
+        Predicate<Person> predicate = (person) -> person.name.equals("Bob");
+        Arrays.stream(getPeopleArray())
+                .filter(person -> person.check(predicate))
+                .map(p -> p.name)
+                .forEach(System.out::println);
+
+        System.out.println("\nNumber of people: " + PeopleFunction.count().apply(getPeopleArray()));
+        System.out.println("Average Age: " + PeopleFunction.average().apply(getPeopleArray()));
+        PeopleFunction.adults().apply(getPeopleArray()).stream().map(p -> p.name).forEach(System.out::println);
     }
 }
