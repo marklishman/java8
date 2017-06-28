d08a7d6f76d5183523b26257fcb875c551c76994
marklishman <mark.lishman@gmail.com>
2017-06-28 19:49:26 +0100

    Organizing files

--- src/main/java/io/lishman/functional/stream/SimpleStream.java
+++ src/main/java/io/lishman/functional/stream/SimpleStream.java
@@ -3,7 +3,7 @@ package io.lishman.functional.stream;
 import java.util.Arrays;
 import java.util.List;
 
-public class SimpleStreamExample {
+public class SimpleStream {
 
     public static void main(String[] args) {
 
